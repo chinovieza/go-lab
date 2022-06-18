8715beed6d07bb850d1929a924a739551853edc8
Chino Smitthikunanon <chinovieza@gmail.com>
2022-06-18 15:28:36 +0700

    Add the first unit testing

---
+++ greetings/greetings_test.go
@@ -0,0 +1,20 @@
+package greetings
+
+import (
+	"regexp"
+	"testing"
+)
+
+// TestHelloName calls greeting.Hello with a name, checking
+// for a valid return value.
+func TestHelloName(t *testing.T) {
+	name := "Gladys"
+	want := regexp.MustCompile(`\b` + name + `\b`)
+	msg, err := Hello("Gladys")
+	if !want.MatchString(msg) || err != nil {
+		t.Fatalf(`Hello("Gladys") = %q, %v, want match for %#q, nil`, msg, err, want)
+	}
+}
+
+// TestingHelloEmpty calls greeting.Hello with empty string,
+// checking for an error.
